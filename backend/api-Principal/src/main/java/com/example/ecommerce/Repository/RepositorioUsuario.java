740d5b4c0279299f0d5aebc7083dc11a80da9dbc
Matias Almaraz <mleandroalmaraz@gmail.com>
2026-03-23 12:31:43 -0300

    cambios query usuario

--- backend/api-Principal/src/main/java/com/example/ecommerce/Repository/RepositorioUsuario.java
+++ backend/api-Principal/src/main/java/com/example/ecommerce/Repository/RepositorioUsuario.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.ecommerce.Dto.UsuarioDto;
 import com.example.ecommerce.Dto.Request.UsuarioRequestDto;
@@ -15,7 +16,7 @@ public interface RepositorioUsuario extends JpaRepository<Usuario,Long> {
     boolean existsByNombre(String nombre);
     boolean existsByCorreo(String email);
     @Query("SELECT u from Usuario u join fetch u.roles where usuario.nombre = :nombre ")
-    Optional<Usuario> findByNombre(String nombre);
+    Optional<Usuario> findByNombre(@Param("nombre") String nombre);
 
    
 
